1c9ac674624a48a084c2c26a8330c21f1c2665ee
francescapetra <francescalaurapetra@gmail.com>
2021-05-07 15:38:36 +0200

    google font

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
     <title>Home</title>
--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -2,6 +2,7 @@
     <div class="container">
         <div class="red logo_container">
             <img class="logo_nav" src="{{ asset('img/marchio-sito-test.png') }}" alt="logo_molisana">
+
         </div>
     </div>
 </header>

a4a67fe0e206f26c031b80f00fe1bede18187a01
francescapetra <francescalaurapetra@gmail.com>
2021-05-07 16:28:55 +0200

    nav bar

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -2,7 +2,24 @@
     <div class="container">
         <div class="red logo_container">
             <img class="logo_nav" src="{{ asset('img/marchio-sito-test.png') }}" alt="logo_molisana">
-
         </div>
+        <nav class="main_nav">
+            <ul class="ul_nav">
+                
+               @foreach ($menu_link as $link)
+               
+                   @if ($loop -> first)
+                    <li class="li_nav">
+                      <a class=" test" href="">{{$link}}</a> </li> 
+                    @else
+                    <li class="li_nav">
+                    <a class="link_nav" href="">{{$link}}</a>
+                </li> 
+                    @endif
+
+               @endforeach
+
+            </ul>
+        </nav>
     </div>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $menu_link = [
+        "Home",
+        "Prodotti",
+        "News"
+    ];
+    return view('home', [
+        "menu_link"=> $menu_link]
+    );
 });
+// "menu_link"=> 
